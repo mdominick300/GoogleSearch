394b8e57a2ba5d9498563c9c911dcf246593a075
Matt Dominick <mdominick300@gmail.com>
2020-03-18 20:44:11 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Landing from './pages/Landing';
+import Navbar from './components/Navbar';
 import Search from './pages/Search';
 import Saved from './pages/Saved';
 
@@ -9,9 +10,10 @@ import Saved from './pages/Saved';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path="/" component = {Landing} />
-      <Route exact path="/profile/:id" component = {Search} />
-      <Route exact path="/buy" component = {Saved} />
+      <Route exact path="/search" component = {Search} />
+      <Route exact path="/saved" component = {Saved} />
       {/* include route with Id for specific item detail page */}
     </Router>
   );
