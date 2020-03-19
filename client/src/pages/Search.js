754b8350d96e0b3d5f7f70d7c6d86cbfb8bca267
Matt Dominick <mdominick300@gmail.com>
2020-03-19 19:36:44 -0400

    display

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import { List, ListItem } from "../components/List";
+import DeleteBtn from "../components/DeleteBtn";
+import { Link } from "react-router-dom";
 
-function Search(){
-    return(
+
+function Search() {
+    // Setting our component's initial state
+    const [books, setBooks] = useState([])
+
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+
+    function loadBooks() {
+        API.getBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
+    //   Deletes a book from the database with a given id, then reloads books from the db
+    function deleteBook(id) {
+        API.deleteBook(id)
+            .then(res => loadBooks())
+            .catch(err => console.log(err));
+    }
+    return (
         <>
-        <h1>search</h1>
+            <h1>search</h1>
+            <List>
+                {books.map(book => (
+                    <ListItem key={book._id}>
+                        <Link to={"/books/" + book._id}>
+                            <strong>
+                                {book.title} Photo: <img width="200px" height="200px" src={require = (book.image)} />
+                            </strong>
+                        </Link>
+                        <DeleteBtn onClick={() => deleteBook(book._id)} />
+                    </ListItem>
+                ))}
+            </List>
         </>
     )
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getListings: function() {
-    return axios.get("/api/listings");
+  getBooks: function() {
+    return axios.get("/api/books");
   },
   // Gets the book with the given id
-  getListing: function(id) {
-    return axios.get("/api/listings/" + id);
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
   },
   // Deletes the book with the given id
-  deleteListing: function(id) {
-    return axios.delete("/api/listings/" + id);
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
   },
   // Saves a book to the database
-  saveListing: function(listingData) {
-    return axios.post("/api/listings", listingData);
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
   }
 };
