f66e6698fc0d6289f73df8a7a892b48a4a913609
Matt Dominick <mdominick300@gmail.com>
2020-03-20 21:57:11 -0400

    api call

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,47 +1,69 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import { List, ListItem } from "../components/List";
-import DeleteBtn from "../components/DeleteBtn";
-import { Link } from "react-router-dom";
+import SaveBtn from "../components/SaveBtn";
+import Axios from "axios";
+import axios from "axios";
 
 
 function Search() {
     // Setting our component's initial state
-    const [books, setBooks] = useState([])
+    const [book, setBook] = useState("")
+    const [result, setResult] = useState([])
+    const [apiKey, setApiKey] = useState("AIzaSyAgfefI0jY5WxchNeuMSXWJK5z-vOLkSyw")
 
     // Load all books and store them with setBooks
-    useEffect(() => {
-        loadBooks()
-    }, [])
+    // useEffect(() => {
+    //     loadBooks()
+    // }, [])
 
-
-    function loadBooks() {
-        API.getBooks()
-            .then(res =>
-                setBooks(res.data)
-            )
-            .catch(err => console.log(err));
-    };
-
-    //   Deletes a book from the database with a given id, then reloads books from the db
-    function deleteBook(id) {
-        API.deleteBook(id)
-            .then(res => loadBooks())
-            .catch(err => console.log(err));
+    function handleChange(event){
+        const book = event.target.value;
+        setBook(book);
     }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40")
+        .then(data => {
+            console.log(data.data.items);
+            setResult(data.data.items);
+        })
+    }
+
+    function saveBook(){
+        console.log(book);
+    
+
+    }
+
     return (
         <>
-            <h1>search</h1>
+            <h1>Search for Books</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} placeholder="Search for books"/>
+                <button type="submit" className="button">Search</button>
+            </form>
             <List>
-                {books.map(book => (
-                    <ListItem key={book._id}>
-                        <Link to={"/books/" + book._id}>
-                            <strong>
-                                {book.title} Photo: <img width="200px" height="200px" src={require = (book.image)} />
-                            </strong>
-                        </Link>
-                        <DeleteBtn onClick={() => deleteBook(book._id)} />
-                    </ListItem>
+                {result.map(book => (
+                    <div className="columns">
+                        <div className="column is-four-fifths">
+                              <h1>{book.volumeInfo.title}</h1>
+                              <div className="columns">
+                              <div className="column is-half"> 
+                              <img width="200px" height="200px" src={book.volumeInfo.imageLinks.thumbnail}/>
+                              </div>
+                              <div className="column is-half"> 
+                                <p>{book.volumeInfo.authors}</p>
+                                <p>{book.volumeInfo.description}</p>
+                                </div>
+                                </div>
+                                
+                        </div>
+                        <div className="column is-four-fifths">
+                        <SaveBtn onClick={() => saveBook(book._id)} />
+                        </div>
+                    </div>
                 ))}
             </List>
         </>
